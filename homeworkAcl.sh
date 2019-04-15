1f5732e6abe9b2b8c4fd6f590964e5cf762c240e
root <root@ip-172-31-12-164.eu-north-1.compute.internal>
2019-04-15 15:09:40 +0000

    Homework #1

---
+++ homeworkAcl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# DevOps training #7
+#
+# Homework #1: ACL Task   
+#
+# Author: Pereskokov Vladimir 
+#
+
+PROJECTS=(Proj1 Proj2 Proj3)
+DEVELOPERS=(R1 R2 R3 R4 R5)
+ANALYTICS=(A1 A2 A3 A4)
+INFOMANAGERS=(I1 I2 I3)
+
+umask 0026
+mkdir /projects
+cd /projects
+
+for proj in ${PROJECTS[@]}
+       do
+            mkdir $proj
+       done	       
+
+
+#declare -p 
+
+exit 0
