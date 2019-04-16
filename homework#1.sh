159c859f7bb170028678a30bd9489a5b4b015c6d
Vladimir Pereskokov <markule@mail.ru>
2019-04-16 12:47:30 +0000

    Task 1 Done

--- homework#1.sh
+++ homework#1.sh
@@ -7,24 +7,23 @@
 # Author: Pereskokov Vladimir 
 
 # Environments
-PROJECTS=(proj1 proj2 proj3)
-DEVELOPERS=(r1 r2 r3 r4 r5)
+PROJECTS=(proj1 proj2 proj3) 
+DEVELOPERS=(r1 r2 r3 r4 r5)   
 ANALYTICS=(a1 a2 a3 a4)
 INFOMANAGERS=(i1 i2 i3)
 ALLUSERS=( ${DEVELOPERS[@]} ${ANALYTICS[@]} ${INFOMANAGERS[@]} )
 PASSWORD="1234"
 
-# RW rights
+# Users with RW rights
 PROJ1RW=(r2 r3 r5 a1)
 PROJ2RW=(r1 r5 a1)
 PROJ3RW=(r1 r2 r4 a2)
 
-# R rights in ACL
+# Users with R rights 
 PROJ1R=(a4)
 PROJ2R=(a2 a3)
 PROJ3R=(a1 a4)
 
-#umask 0026
 mkdir /projects
 cd /projects
 
@@ -46,36 +45,36 @@ for proj in ${PROJECTS[@]}
             
             case $proj in
             proj1)
-		# RW rights
+		# RW rights by Group
 	        for u in ${PROJ1RW[@]}
 	        do		
                     adduser $u $proj
 	        done
-		# R rights
+		# R rights by ACL
 		for u in ${PROJ1R[@]}
 		do	
 	            setfacl -R -m u:$u:r-x $proj
                 done		    
                 ;;
             proj2)
-	        # RW rights	    
+	        # RW rights by Group	    
                 for u in ${PROJ2RW[@]}
                 do
                     adduser $u $proj
                 done
-		# R rights
+		# R rights by ACL
                 for u in ${PROJ2R[@]}
 		do
 		    setfacl -R -m u:$u:r-x $proj
 	        done	    
                 ;;
             proj3)
-		# RW rights    
+		# RW rights by Group   
                 for u in ${PROJ3RW[@]}
                 do
                 adduser $u $proj
                 done
-		# R rights
+		# R rights by ACL
 		for u in ${PROJ3R[@]}
 		do
 	            setfacl -R -m u:$u:r-x $proj
@@ -84,8 +83,5 @@ for proj in ${PROJECTS[@]}
            esac
 
        done
-       
-
-#declare -p ALLUSERS
 
 exit 0
